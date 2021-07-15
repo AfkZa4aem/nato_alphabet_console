d9ec06660955fe3f391ef785e7719501dc54fea7
Maxim Sereda <AfkZa4aem@users.noreply.github.com>
2021-07-15 15:03:19 +0300

    add errors handling

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import pandas
 
-
 data = pandas.read_csv("./nato_phonetic_alphabet.csv")
-alphabet_dict = {row.letter:row.code for (index, row) in data.iterrows()}
+alphabet_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(alphabet_dict)
 
-user_input = (input("Enter a word: ")).upper()
-word = [letter for letter in list(user_input)]
-result = [alphabet_dict[letter] for letter in word if alphabet_dict[letter]]
 
-print(result)
+def generate_list():
+    user_input = (input("Enter a word: ")).upper()
+    try:
+        result = [alphabet_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_list()
+    else:
+        print(result)
+
+
+generate_list()
